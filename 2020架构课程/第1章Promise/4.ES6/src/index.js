0eb711686b4ed99f29b2ad320333d0a739df752a
mac <mac@macdeMacBook-Air-7.local>
2020-05-31 19:47:57 +0800

    提交代码

---
+++ 2020架构课程/第1章Promise/4.ES6/src/index.js
@@ -0,0 +1,22 @@
+// ES6中的模块化问题
+// 模块化解决的问题:命名冲突(命名空间)
+
+// node中自带的模块化功能 require module.exports commonjs规范
+
+// let obj = {
+//     a(){
+
+//     }
+// }
+
+// let obj1 = {
+//     a(){
+
+//     }
+// }
+
+
+
+
+
+document.write('hello');
